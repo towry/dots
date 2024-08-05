8f3e9f9755314d767e5d8a17b6d527dd26003a35
Towry Wang <towry@users.noreply.github.com>
2024-08-05 11:05:01 +0800

    fix: shell

--- nix/hm/skhd.nix
+++ nix/hm/skhd.nix
@@ -208,7 +208,7 @@
        let wid=$(yabai -m query --windows | jq "[.[] | select(.app == \"$1\") | .id][0]")
        echo "$wid"
 
-       if [[ "$wid" -eq "0" ]] && [[ "$2" != ""]]; then
+       if [[ "$wid" -eq "0" ]] && [[ "$2" != "" ]]; then
       $2
        else
       yabai -m window --focus "$wid"
