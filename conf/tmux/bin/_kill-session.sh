16118d29d857e408b26b1fc812da44493dd98749
Towry Wang <towry@users.noreply.github.com>
2024-08-05 11:28:33 +0800

    refactor: rename default session name

--- conf/tmux/bin/_kill-session.sh
+++ conf/tmux/bin/_kill-session.sh
@@ -3,7 +3,7 @@
 ## then switch back to session '0' and kill this session
 
 current_session=$(tmux display-message -p | sed -e 's/^\[//' -e 's/\].*//')
-if [[ "$current_session" != "0" ]]; then
-  tmux switch-client -t "0"
+if [[ "$current_session" != "Main" ]]; then
+  tmux switch-client -t "Main"
   tmux kill-session -t "$current_session"
 fi
