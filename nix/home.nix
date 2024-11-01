a546a934641df8858c73b1bd18f7166a133fadd6
Towry Wang <towry@users.noreply.github.com>
2024-11-01 18:15:20 +0800

    feat: enable yabai

--- nix/home.nix
+++ nix/home.nix
@@ -165,8 +165,8 @@ in {
     ./hm/kitty.nix
     ./hm/zellij.nix
     ./hm/rust.nix
-    # ./hm/skhd.nix
-    # ./hm/yabai.nix
+    ./hm/skhd.nix
+    ./hm/yabai.nix
     ./hm/dark-mode-notify.nix
   ];
 
