a5358ffb8f14a9a2f03681fd4dab4576527b155b
Towry Wang <towry@users.noreply.github.com>
2024-11-17 14:39:51 +0800

    fix: set some var

--- nix/modules/elixir.nix
+++ nix/modules/elixir.nix
@@ -3,7 +3,18 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  mk_elixir_nix_version = elixir_version: builtins.replaceStrings ["."] ["_"] "elixir_${elixir_version}";
+  beamPackages =
+    pkgs.beam.packagesWith
+    pkgs.beam.interpreters."erlang_${config.elixir.erlang_nix_version}";
+  erlang = beamPackages.erlang;
+  elixir = beamPackages.${mk_elixir_nix_version config.elixir.elixir_nix_version};
+  elixir-ls = beamPackages.elixir-ls.overrideAttrs (_oldAttrs: {elixir = elixir;});
+  hex = beamPackages.hex;
+  # use rebar from nix instead of fetch externally
+  rebar3 = beamPackages.rebar3;
+in {
   options.elixir = {
     enable = lib.mkEnableOption {
       default = false;
@@ -22,23 +33,17 @@
   };
 
   config = lib.mkIf config.elixir.enable {
-    home.packages = let
-      mk_elixir_nix_version = elixir_version: builtins.replaceStrings ["."] ["_"] "elixir_${elixir_version}";
-      beamPackages =
-        pkgs.beam.packagesWith
-        pkgs.beam.interpreters."erlang_${config.elixir.erlang_nix_version}";
-      erlang = beamPackages.erlang;
-      elixir = beamPackages.${mk_elixir_nix_version config.elixir.elixir_nix_version};
-      elixir-ls = beamPackages.elixir-ls.overrideAttrs (_oldAttrs: {elixir = elixir;});
-      hex = beamPackages.hex;
-      # use rebar from nix instead of fetch externally
-      rebar3 = beamPackages.rebar3;
-    in [
+    home.packages = [
       elixir
       erlang
       elixir-ls
       hex
       rebar3
     ];
+    home.sessionVariables = {
+      ELS_INSTALL_PREFIX = "${elixir-ls}/lib";
+      MIX_PATH = "${hex}/lib/erlang/lib/hex/ebin";
+      MIX_REBAR3 = "${rebar3}/bin/rebar3";
+    };
   };
 }
