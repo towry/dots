2c7d2342b69e4251cc89386c0d6516af2d615737
Towry Wang <towry@users.noreply.github.com>
2024-11-19 09:19:03 +0800

    fix: dark mode

--- nix/pkgs/switch-theme/default.nix
+++ nix/pkgs/switch-theme/default.nix
@@ -1,14 +1,23 @@
 {pkgs, ...}: let
-  python = import ../../lib/python3.nix { inherit pkgs; };
-  in
+  python = import ../../lib/python3.nix {inherit pkgs;};
+in
   pkgs.writeShellApplication {
     name = "switch-theme";
     runtimeInputs = with pkgs; [coreutils neovim-remote python];
-    text = ''
-      #!${pkgs.bash}/bin/bash
+    text =
+      if pkgs.system == "aarch64-darwin"
+      then ''
+        #!${pkgs.bash}/bin/bash
 
-      export PATH="$PATH:${pkgs.neovim-remote}/bin:$HOME/.nix-profile/bin"
+        export PATH="$PATH:${pkgs.neovim-remote}/bin:$HOME/.nix-profile/bin:/run/current-system/sw/bin"
 
-      ${pkgs.dark-notify}/bin/dark-notify -c "PATH=$PATH:${pkgs.neovim-remote}/bin ${python}/bin/python3 ${./switch-theme.py}" 2>&1
-    '';
+        ${pkgs.dark-notify}/bin/dark-mode-notify -c "PATH=$PATH:${pkgs.neovim-remote}/bin ${python}/bin/python3 ${./switch-theme.py}" 2>&1
+      ''
+      else ''
+        #!${pkgs.bash}/bin/bash
+
+        export PATH="$PATH:${pkgs.neovim-remote}/bin:$HOME/.nix-profile/bin:/run/current-system/sw/bin"
+
+        ${pkgs.dark-notify}/bin/dark-notify -c "PATH=$PATH:${pkgs.neovim-remote}/bin ${python}/bin/python3 ${./switch-theme.py}" 2>&1
+      '';
   }
