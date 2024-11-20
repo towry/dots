3a26b509fa76f4515783c5c4fafff94fecc04fd1
Towry Wang <towry@users.noreply.github.com>
2024-11-20 09:27:24 +0800

    feat: dark notify aarch64

--- nix/pkgs/dark-mode-notify.nix
+++ nix/pkgs/dark-mode-notify.nix
@@ -1,35 +1,45 @@
-{pkgs, ...}:
-pkgs.stdenv.mkDerivation rec {
-  name = "dark-notify";
+{pkgs, ...}: let
   version = "0.1.2";
-
-  src = pkgs.fetchurl {
-    url = "https://github.com/cormacrelf/dark-notify/releases/download/v${version}/dark-notify-v0.1.2.tar.gz";
-    sha256 = "987c4e40ca9f7996f72d8967a74417e2fc7e8d7aea02e93bd314f80a80817f9a";
+  tarballUrlMap = {
+    x86_64-darwin = {
+      url = "https://github.com/cormacrelf/dark-notify/releases/download/v${version}/dark-notify-v0.1.2.tar.gz";
+      sha256 = "987c4e40ca9f7996f72d8967a74417e2fc7e8d7aea02e93bd314f80a80817f9a";
+    };
+    aarch64-darwin = {
+      url = "https://github.com/pze/dark-notify/releases/download/v0.1.2/dark-notify-aarch64-darwin-v0.1.2.tar.gz";
+      sha256 = "0kf1ms88847j022lk7gw26zjxw9cvv3zz8xar88l0vx8f46xjss6";
+    };
   };
+  tarball = tarballUrlMap.${pkgs.system};
+in
+  pkgs.stdenv.mkDerivation {
+    name = "dark-notify";
+    version = version;
 
-  nativeBuildInputs = [
-    # installShellFiles
-    pkgs.makeBinaryWrapper
-  ];
+    src = pkgs.fetchurl tarball;
 
-  sourceRoot = ".";
-  installPhase = ''
-    mkdir -p $out/bin
-    mv dark-notify $out/bin/dark-notify
-  '';
+    nativeBuildInputs = [
+      # installShellFiles
+      pkgs.makeBinaryWrapper
+    ];
 
-  postFixup = with pkgs; ''
-    wrapProgram $out/bin/dark-notify \
-      --set PATH ${lib.makeBinPath [
-      coreutils
-      bash
-      python3
-    ]}
-  '';
+    sourceRoot = ".";
+    installPhase = ''
+      mkdir -p $out/bin
+      mv dark-notify $out/bin/dark-notify
+    '';
 
-  meta = {
-    description = "Watcher for macOS 10.14+ light/dark mode changes";
-    homepage = "https://github.com/cormacrelf/dark-notify";
-  };
-}
+    postFixup = with pkgs; ''
+      wrapProgram $out/bin/dark-notify \
+        --set PATH ${lib.makeBinPath [
+        coreutils
+        bash
+        python3
+      ]}
+    '';
+
+    meta = {
+      description = "Watcher for macOS 10.14+ light/dark mode changes";
+      homepage = "https://github.com/cormacrelf/dark-notify";
+    };
+  }
