4ef792c4a9c5fda923a4dcb1c71d1994ad3cf7ab
Towry Wang <towry@users.noreply.github.com>
2024-12-25 10:30:34 +0800

    fix: change lazy config

--- nix/hm/lazygit.nix
+++ nix/hm/lazygit.nix
@@ -14,7 +14,12 @@
         showFileTree = false;
         commitHashLength = 6;
         showDivergenceFromBaseBranch = "arrowAndNumber";
+        enlargedSideViewLocation = "top";
+        showBottomLine = false;
+        commandLogSize = 4;
+        expandFocusedSidePanel = true;
         nerdFontsVersion = "3";
+        windowSize = "half"; # "full" | "half" | "default"
         border = "single";
         animateExplosion = false;
         portraitMode = "auto"; # "never" | "always" | "auto"
@@ -40,20 +45,33 @@
       };
       os = {
         edit = "nvim";
-        editAtLine = "{{editor}} +{{line}} {{filename}}";
+        editPreset = "nvim-remote";
+        editAtLine = "nvim +{{line}} {{filename}}";
       };
       # https://github.com/jesseduffield/lazygit/blob/master/docs/Custom_Command_Keybindings.md
       customCommands = [
         {
           key = "H";
           context = "commits";
-          command = "gh browse {{.SelectedLocalCommit.Hash}}";
+          command = "gh browse {{.SelectedCommit.Sha}}";
+        }
+        # in diff view, open file in nvim at selected commit
+        {
+          key = "E";
+          context = "commitFiles";
+          command = "nvim -c 'Gedit {{.SelectedCommit.Sha}}:{{.SelectedFile.Name}}'";
         }
       ];
-      quitOnTopLevelReturn = true;
+      quitOnTopLevelReturn = false;
       disableStartupPopups = true;
-      promptToReturnFromSubprocess = true;
-      notARepository = "skip";
+      promptToReturnFromSubprocess = false;
+      notARepository = "quit";
+      keybinding = {
+        universal = {
+          quit = "<c-q>";
+          quitWithoutChangingDirectory = "Q";
+        };
+      };
     };
   };
 }
