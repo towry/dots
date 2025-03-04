de4b22de1e812c52ff92d027aa3a39cf73f1d26d
Towry Wang <towry@users.noreply.github.com>
2025-03-04 22:22:34 +0800

    add git-sim

--- nix/hm/git.nix
+++ nix/hm/git.nix
@@ -1,7 +1,7 @@
 {
   pkgs,
   lib,
-  theme,
+  # theme,
   ...
 }:
 let
@@ -25,11 +25,10 @@ in
   home.packages = with pkgs; [
     # github cli, manage repo, gists etc.
     gh
-    stgit
+    # stgit
     git-smash
-    # difftastic
     gnupg
-    # gitu
+    git-sim
   ];
   programs.git = {
     enable = true;
@@ -44,127 +43,121 @@ in
       background = "light";
     };
 
-    aliases =
-      {
-        co = "checkout";
-        ad = "add";
-        ada = "add -A";
-        sw = "switch";
-        # apply patch with commit text only (no committee info)
-        apply-diff-patch = "apply --allow-empty";
-        # amend without edit, not use ca because sometimes we should use git sm instead.
-        amend = "commit --amend --no-edit";
-        ci = "commit";
-        cne = "commit --no-edit";
-        cm = "commit -m";
-        st = "status";
-        add-note = ''branch --edit-description'';
-        note = ''!git config --get branch.$(git rev-parse --abbrev-ref HEAD).description'';
-        sh = "show";
-        default-branch = "!git symbolic-ref refs/remotes/origin/HEAD | cut -d'/' -f4";
-        ss = ''!f() { if [ $# -eq 0 ]; then git stash list; else git stash $@; fi; }; f'';
-        # stash and name with "wip"
-        ss-wip = "stash -m wip";
-        # stash staged changes
-        ss-staged = "stash --staged";
-        up = ''!git push -u origin HEAD:$(git rev-parse --abbrev-ref HEAD)'';
-        fu = ''!git status --short && git push -u --force-with-lease'';
-        pr = "pull --rebase";
-        pp = ''!git pull --no-tags --prune --ff origin $(git rev-parse --abbrev-ref HEAD)'';
-        px = ''pull --all --prune --no-tags'';
-        pp-theirs = ''!git pull -X theirs --no-tags --ff origin $(git rev-parse --abbrev-ref HEAD)'';
-        pp-ours = ''!git pull -X ours --no-tags --ff origin $(git rev-parse --abbrev-ref HEAD)'';
-        ps = ''!git pull --autostash --no-tags origin $(git rev-parse --abbrev-ref HEAD)'';
-        pf = ''!git pull --no-tags --ff-only $(git-rev-parse --abbrev-ref HEAD)'';
-        fa = "fetch --all --no-tags";
-        # fz = "fuzzy";
-        ff = "fetch --no-tags";
-        fp = "fetch --prune --prune-tags";
-        mg = "merge --no-edit --ff";
-        mg-theirs = "merge --no-edit --ff -X theirs";
-        kill-merge = "merge --abort";
-        br = "branch --sort=-committerdate";
-        br-gone = "!git branch -vv | grep -F ': gone]' | awk '{ print $1 }' | grep -vF '*'";
-        br-prune-gone = "!git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '$2 == \"[gone]\" {print $1}' | xargs -r git branch -D";
-        df = "diff";
-        dt = "difftool";
-        dfc = "diff --cached";
-        dfh = "diff HEAD";
-        dfs = "diff --stat";
-        wc = "whatchanged";
-        wc1 = "whatchanged -1";
-        wcl = "whatchanged -3";
-        cat = "cat-file -p";
-        undo = "reset --soft";
-        sm = "!git-smash";
-        sn = "!git-smash --no-rebase";
-        fixup = "commit --fixup";
-        autofixup = "!git commit --fixup $1 && git rebase -i --autosquash --rebase-merges $1~1";
-        reset-remote = "!git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)";
-        unstage = "restore --staged";
-        changed-files = "!sh -c 'default_branch=master; if git rev-parse --verify master >/dev/null 2>/dev/null; then default_branch=master; elif git rev-parse --verify main >/dev/null 2>/dev/null; then default_branch=main; else echo \"Neither master nor main branches found.\"; exit 1; fi; git fetch origin $default_branch >/dev/null 2>&1; git diff --name-only origin/$default_branch...'";
-        add-remote-branches = "remote set-branches --add";
-        ls-remote-heads = "!git ls-remote -h -q |  awk '{print $2}' | sed 's/refs\\/heads\\///'";
-        lg = "log --graph --pretty=format:'%Cred%h%Creset %s %C(white)%ad%Creset %C(yellow)%d%Creset %C(bold blue)<%an>%Creset' --date=short";
-        lg1 = "log --oneline --date=relative --pretty=format:'%Cred%h%Creset %s %C(white)%ad%Creset %C(yellow)%d%Creset %C(bold blue)<%an>%Creset'";
-        lt = "log --oneline --date=relative -n3 --pretty=format:'%Cred%h%Creset %s %C(yellow)%d%Creset %C(bold blue)<%an>%Creset - %C(white)%ad%Creset'";
-        yesterday = "log --since yesterday --until=midnight --color --graph \
+    aliases = {
+      co = "checkout";
+      ad = "add";
+      ada = "add -A";
+      sw = "switch";
+      # apply patch with commit text only (no committee info)
+      apply-diff-patch = "apply --allow-empty";
+      # amend without edit, not use ca because sometimes we should use git sm instead.
+      amend = "commit --amend --no-edit";
+      ci = "commit";
+      cne = "commit --no-edit";
+      cm = "commit -m";
+      st = "status";
+      add-note = ''branch --edit-description'';
+      note = ''!git config --get branch.$(git rev-parse --abbrev-ref HEAD).description'';
+      sh = "show";
+      default-branch = "!git symbolic-ref refs/remotes/origin/HEAD | cut -d'/' -f4";
+      ss = ''!f() { if [ $# -eq 0 ]; then git stash list; else git stash $@; fi; }; f'';
+      # stash and name with "wip"
+      ss-wip = "stash -m wip";
+      # stash staged changes
+      ss-staged = "stash --staged";
+      up = ''!git push -u origin HEAD:$(git rev-parse --abbrev-ref HEAD)'';
+      fu = ''!git status --short && git push -u --force-with-lease'';
+      pr = "pull --rebase";
+      pp = ''!git pull --no-tags --prune --ff origin $(git rev-parse --abbrev-ref HEAD)'';
+      px = ''pull --all --prune --no-tags'';
+      pp-theirs = ''!git pull -X theirs --no-tags --ff origin $(git rev-parse --abbrev-ref HEAD)'';
+      pp-ours = ''!git pull -X ours --no-tags --ff origin $(git rev-parse --abbrev-ref HEAD)'';
+      ps = ''!git pull --autostash --no-tags origin $(git rev-parse --abbrev-ref HEAD)'';
+      pf = ''!git pull --no-tags --ff-only $(git-rev-parse --abbrev-ref HEAD)'';
+      fa = "fetch --all --no-tags";
+      # fz = "fuzzy";
+      ff = "fetch --no-tags";
+      fp = "fetch --prune --prune-tags";
+      mg = "merge --no-edit --ff";
+      mg-theirs = "merge --no-edit --ff -X theirs";
+      kill-merge = "merge --abort";
+      br = "branch --sort=-committerdate";
+      br-gone = "!git branch -vv | grep -F ': gone]' | awk '{ print $1 }' | grep -vF '*'";
+      br-prune-gone = "!git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '$2 == \"[gone]\" {print $1}' | xargs -r git branch -D";
+      df = "diff";
+      dt = "difftool";
+      dfc = "diff --cached";
+      dfh = "diff HEAD";
+      dfs = "diff --stat";
+      wc = "whatchanged";
+      wc1 = "whatchanged -1";
+      wcl = "whatchanged -3";
+      cat = "cat-file -p";
+      undo = "reset --soft";
+      sm = "!git-smash";
+      sn = "!git-smash --no-rebase";
+      fixup = "commit --fixup";
+      autofixup = "!git commit --fixup $1 && git rebase -i --autosquash --rebase-merges $1~1";
+      reset-remote = "!git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)";
+      unstage = "restore --staged";
+      changed-files = "!sh -c 'default_branch=master; if git rev-parse --verify master >/dev/null 2>/dev/null; then default_branch=master; elif git rev-parse --verify main >/dev/null 2>/dev/null; then default_branch=main; else echo \"Neither master nor main branches found.\"; exit 1; fi; git fetch origin $default_branch >/dev/null 2>&1; git diff --name-only origin/$default_branch...'";
+      add-remote-branches = "remote set-branches --add";
+      ls-remote-heads = "!git ls-remote -h -q |  awk '{print $2}' | sed 's/refs\\/heads\\///'";
+      lg = "log --graph --pretty=format:'%Cred%h%Creset %s %C(white)%ad%Creset %C(yellow)%d%Creset %C(bold blue)<%an>%Creset' --date=short";
+      lg1 = "log --oneline --date=relative --pretty=format:'%Cred%h%Creset %s %C(white)%ad%Creset %C(yellow)%d%Creset %C(bold blue)<%an>%Creset'";
+      lt = "log --oneline --date=relative -n3 --pretty=format:'%Cred%h%Creset %s %C(yellow)%d%Creset %C(bold blue)<%an>%Creset - %C(white)%ad%Creset'";
+      yesterday = "log --since yesterday --until=midnight --color --graph \
             --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)[%an]%Creset' \
             --abbrev-commit";
-        clone1 = "clone --depth=1";
-        des = "describe";
-        wt = "worktree";
-        mt = "mergetool";
-        mt-code = "mergetool --tool=code";
-        mt-cursor = "mergetool --tool=cursor";
-        bi = "bisect";
-        rp = "rev-parse";
-        rl = "rev-list";
-        rn = "name-rev";
-        rb = "rebase";
-        rbk = "rebase --skip";
-        rbc = "rebase --continue";
-        rba = "rebase --abort";
-        rbi = "rebase -i";
-        # autosquash previous fixup! commits
-        close-fixup = "!git rebase -i --autosquash --autostash";
-        "re3" = "rerere";
-        latest-tag = "describe --tags";
-        alias = "! git config --get-regexp '^alias\\.' | sed 's/^alias\\.//' | fzf";
-        ignore = "!gi() { curl -sL https://www.gitignore.io/api/$@ ;}; gi";
-        config-fetch-origin = ''config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"'';
-        clone-bare = ''!f() { git clone --bare $@ && cd $@ && git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"; }; f'';
-        ahead = "rev-list --left-right --count";
-        # count commit diff between two branches
-        commit-diff-count = ''!f() { git rev-list --count HEAD ^$1; }; f'';
-        # create feat branch in format of feat/YYYYMMDD-short-description, also accept other git arguments
-        br-feat = "!f() { git checkout -b feat/$(date +%Y%m)-$1 $2; }; f";
-        br-fix = "!f() { git checkout -b fix/$(date +%Y%m)-$1 $2; }; f";
-        br-refactor = "!f() { git checkout -b refactor/$(date +%Y%m)-$1 $2; }; f";
-        # sync and rebase, for example: git sync-rebase origin master do: git fetch origin master && git rebase origin/master
-        sync-rebase = "!f() { git fetch $1 $2 && git rebase $1/$2; }; f";
-        # commit convention
-        it-wip = ''!f() { git commit -m "wip: [skip ci] $([[ -z $@ ]] && date || echo $@ )"; }; f'';
-        it-fix = ''!f() { git commit -m "fix: $(echo $@)"; }; f'';
-        it-fmt = ''!f() { git commit -m "style: $(echo $@)"; }; f'';
-        it-test = ''!f() { git commit -m "test: $(echo $@)"; }; f'';
-        it-ref = ''!f() { git commit -m "refactor: $(echo $@)"; }; f'';
-        it-doc = ''!f() { git commit -m "doc: $(echo $@)"; }; f'';
-        it-feat = ''!f() { git commit -m "feat: $(echo $@)"; }; f'';
-        it-perf = ''!f() { git commit -m "perf: $(echo $@)"; }; f'';
-        it-chore = ''!f() { git commit -m "chore: $(echo $@)"; }; f'';
-        it-revert = ''!f() { git commit -m "revert: $(echo $@)"; }; f'';
-        it-build = ''!f() { git commit -m "build: $(echo $@)"; }; f'';
-        it-ci = ''!f() { git commit -m "ci: $(echo $@)"; }; f'';
-        it-skip = ''!f() { git commit -m "[skip ci]: $(echo $@)"; }; f'';
-        it-deps = ''!f() { git commit -m "deps: $(echo $@)"; }; f'';
-        it-rm = ''!f() { git commit -m "cleanup: $(echo $@)"; }; f'';
-      }
-      // {
-        # alias for stg
-        refresh = "!f() { stg refresh $@; }; f";
-        list = "!f() { stg series $@; }; f";
-      };
+      clone1 = "clone --depth=1";
+      des = "describe";
+      wt = "worktree";
+      mt = "mergetool";
+      mt-code = "mergetool --tool=code";
+      mt-cursor = "mergetool --tool=cursor";
+      bi = "bisect";
+      rp = "rev-parse";
+      rl = "rev-list";
+      rn = "name-rev";
+      rb = "rebase";
+      rbk = "rebase --skip";
+      rbc = "rebase --continue";
+      rba = "rebase --abort";
+      rbi = "rebase -i";
+      # autosquash previous fixup! commits
+      close-fixup = "!git rebase -i --autosquash --autostash";
+      "re3" = "rerere";
+      latest-tag = "describe --tags";
+      alias = "! git config --get-regexp '^alias\\.' | sed 's/^alias\\.//' | fzf";
+      ignore = "!gi() { curl -sL https://www.gitignore.io/api/$@ ;}; gi";
+      config-fetch-origin = ''config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"'';
+      clone-bare = ''!f() { git clone --bare $@ && cd $@ && git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"; }; f'';
+      ahead = "rev-list --left-right --count";
+      # count commit diff between two branches
+      commit-diff-count = ''!f() { git rev-list --count HEAD ^$1; }; f'';
+      # create feat branch in format of feat/YYYYMMDD-short-description, also accept other git arguments
+      br-feat = "!f() { git checkout -b feat/$(date +%Y%m)-$1 $2; }; f";
+      br-fix = "!f() { git checkout -b fix/$(date +%Y%m)-$1 $2; }; f";
+      br-refactor = "!f() { git checkout -b refactor/$(date +%Y%m)-$1 $2; }; f";
+      # sync and rebase, for example: git sync-rebase origin master do: git fetch origin master && git rebase origin/master
+      sync-rebase = "!f() { git fetch $1 $2 && git rebase $1/$2; }; f";
+      # commit convention
+      it-wip = ''!f() { git commit -m "wip: [skip ci] $([[ -z $@ ]] && date || echo $@ )"; }; f'';
+      it-fix = ''!f() { git commit -m "fix: $(echo $@)"; }; f'';
+      it-fmt = ''!f() { git commit -m "style: $(echo $@)"; }; f'';
+      it-test = ''!f() { git commit -m "test: $(echo $@)"; }; f'';
+      it-ref = ''!f() { git commit -m "refactor: $(echo $@)"; }; f'';
+      it-doc = ''!f() { git commit -m "doc: $(echo $@)"; }; f'';
+      it-feat = ''!f() { git commit -m "feat: $(echo $@)"; }; f'';
+      it-perf = ''!f() { git commit -m "perf: $(echo $@)"; }; f'';
+      it-chore = ''!f() { git commit -m "chore: $(echo $@)"; }; f'';
+      it-revert = ''!f() { git commit -m "revert: $(echo $@)"; }; f'';
+      it-build = ''!f() { git commit -m "build: $(echo $@)"; }; f'';
+      it-ci = ''!f() { git commit -m "ci: $(echo $@)"; }; f'';
+      it-skip = ''!f() { git commit -m "[skip ci]: $(echo $@)"; }; f'';
+      it-deps = ''!f() { git commit -m "deps: $(echo $@)"; }; f'';
+      it-rm = ''!f() { git commit -m "cleanup: $(echo $@)"; }; f'';
+    };
 
     extraConfig = {
       user = {
