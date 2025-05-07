200ec90e0a376ab52ba9d1b427bf9dd5c2433e8a
Towry Wang <towry@users.noreply.github.com>
2025-05-07 16:20:28 +0800

    change m-w to pick panels

--- nix/hm/tmux.nix
+++ nix/hm/tmux.nix
@@ -284,7 +284,7 @@ in
       set -g status-right-length 300
       set -g status-justify left
       set -g status-style bg=blue,fg=black
-      set-window-option -g status-position top
+      set-window-option -g status-position bottom
       set -g message-style fg=black,bg=blue
       set -g message-command-style fg=black,bg=blue
       # set -g status-style bg=default,fg=yellow
