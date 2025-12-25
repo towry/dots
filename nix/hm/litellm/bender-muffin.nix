b40cfc90e1832a7752d0c19d56c4f3e16865768d
Towry Wang <towry@users.noreply.github.com>
2025-12-25 20:16:06 +0800

    up

--- nix/hm/litellm/bender-muffin.nix
+++ nix/hm/litellm/bender-muffin.nix
@@ -8,34 +8,34 @@ let
   modelName = "bender-muffin";
 in
 [
-  # (providers.minimaxClaude.model {
-  #   model_name = modelName;
-  #   litellm_params = {
-  #     model = "anthropic/MiniMax-M2.1";
-  #     max_tokens = 64000;
-  #   };
-  # })
+  (providers.minimaxClaude.model {
+    model_name = modelName;
+    litellm_params = {
+      model = "anthropic/MiniMax-M2.1";
+      max_tokens = 64000;
+    };
+  })
   (providers.zenmuxAnthropic.model {
     model_name = modelName;
     litellm_params = {
       model = "anthropic/anthropic/claude-haiku-4.5";
       max_tokens = 64000;
-      rpm = 1;
-    };
-  })
-  (providers.zenmuxAnthropic.model {
-    model_name = modelName;
-    litellm_params = {
-      model = "anthropic/google/gemini-3-flash-preview";
-      max_tokens = 64000;
       rpm = 2;
     };
   })
-  (providers.opencode.model {
-    model_name = modelName;
-    litellm_params = {
-      model = "openai/glm-4.7-free";
-      max_tokens = 65536;
-    };
-  })
+  # (providers.zenmuxAnthropic.model {
+  #   model_name = modelName;
+  #   litellm_params = {
+  #     model = "anthropic/google/gemini-3-flash-preview";
+  #     max_tokens = 64000;
+  #     rpm = 2;
+  #   };
+  # })
+  # (providers.opencode.model {
+  #   model_name = modelName;
+  #   litellm_params = {
+  #     model = "openai/glm-4.7-free";
+  #     max_tokens = 65536;
+  #   };
+  # })
 ]
